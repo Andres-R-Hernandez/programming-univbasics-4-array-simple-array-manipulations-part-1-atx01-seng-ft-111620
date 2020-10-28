b16237829dca8ac27ea969e2f473b5540f3e35c8
Andres Hernandez <andres.hernandez@utexas.edu>
2020-10-28 16:56:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,14 +10,14 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array)
-  array.pop(2)
+def pop_with_args(array,pop_num)
+  array.pop(pop_num)
 end
 
 def using_shift(array)
   array.shift
 end
 
-def shift_with_args(array)
-  array.shift(2)
+def shift_with_args(array,shift_num)
+  array.shift(shift_num)
 end
